2a9f64dc4099ca9d7df8b8da24bbcda7d735285c
renyawen <849512891@qq.com>
2019-11-03 19:10:59 +0800

    学习springcloud

--- productViewServiceFeign/src/main/java/cn/how2j/springCloud/api/product/controller/ProductController.java
+++ productViewServiceFeign/src/main/java/cn/how2j/springCloud/api/product/controller/ProductController.java
@@ -5,6 +5,8 @@ import cn.how2j.springCloud.api.product.bo.ProductBO;
 import cn.how2j.springCloud.api.product.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,20 @@ import java.util.List;
 
 @Slf4j
 @Controller
+@RefreshScope
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
+    @Value("${version}")
+    String version;
+
     @RequestMapping("/products")
     public Object products(Model m){
         List<ProductBO> ps = productService.listProducts();
         m.addAttribute("ps",ps);
+        m.addAttribute("version",version);
         return "products";
     }
 }
